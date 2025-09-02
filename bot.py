e8df88ee52641521f797c4593bec76184253a6ee
IchoTM <IchoTM@users.noreply.github.com>
2025-09-02 11:05:27 -0400

    Bot will respond to all messages using Googles Gemini AI. Features outside of being a chat bot are still currently very limited.

--- bot.py
+++ bot.py
@@ -1,13 +1,26 @@
 from dotenv import load_dotenv
 from os import getenv
 from telebot import TeleBot
+from google import genai
+from google.genai import types
 
 load_dotenv()
 
 bot = TeleBot(getenv('token'))
+client = genai.Client()
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
     bot.reply_to(message, "Hello, I am AlaX. A general purpose virtual assistant. How can I help you?")
+
+@bot.message_handler(func=lambda msg: True)
+def reply(message):
+    reply = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents= message.text,
+        config=types.GenerateContentConfig(thinking_config=types.ThinkingConfig(thinking_budget=0))
+        )
+    
+    bot.reply_to(message, reply.text)
 
 bot.infinity_polling()
